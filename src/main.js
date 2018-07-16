6d58a5e5ec7b3df2a99a224f97dc9a5b7cfa008c
Aeligo <zoeywang1990@gmail.com>
2018-07-16 10:37:23 +0800

    add slider

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 3000,
+        paginationType:"bullets"
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://img1.qunarzz.com/travel/poi/1807/28/05e1021cd9bdc537.jpg_r_640x290x70_7fc23a19.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'https://img1.qunarzz.com/travel/poi/1807/13/79ad0bfc1d279037.jpg_r_640x290x70_a723d318.jpg'
+      },
+      {
+        id: '0003',
+        imgUrl: 'https://img1.qunarzz.com/travel/poi/1807/ea/4a20a9ec89b6c537.jpg_r_640x290x70_39c9f7ef.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff !important
+.wrapper
+  width :100%
+  height :0
+  overflow :hidden
+  padding-bottom :58.9%
+  background :#ccc
+  .swiper-img
+    width :100%
+</style>
